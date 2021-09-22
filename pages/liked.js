bc256f9cb229b904ed17d69314c73558f33bfbcb
Daniel Lu <daniel020301@gmail.com>
2021-09-21 22:10:59 -0400

    update liked.js

---
+++ pages/liked.js
@@ -0,0 +1,11 @@
+import Head from "next/head";
+import Image from "next/image";
+import { useState, useEffect } from "react";
+import styled from "styled-components";
+import axios from "axios";
+
+const liked = () => {
+    return <div></div>;
+};
+
+export default liked;
